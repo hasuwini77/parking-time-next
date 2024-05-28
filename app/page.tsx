dbd1bb841c37f189435a0be69c617c32cea4e447
Edwin Andermyr <andermyredwin@gmail.com>
2024-05-28 14:45:27 +0200

    adjusted the file structure

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
   return (
     <section className="flex flex-col items-center justify-center">
       <div className="text-center justify-center">
-        <h1 className="text-3xl"> Welcome to Parking Time!</h1>
+        <h1 className="text-3xl"> Welcome to Parking Time!</h1> 
        </div> 
        </section> 
   );
