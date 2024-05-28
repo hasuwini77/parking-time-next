c92d2c032e55eb4b1f039c8ec2e613b2a3ce8a6e
Edwin Andermyr <andermyredwin@gmail.com>
2024-05-28 15:44:44 +0200

    adjusted the tailwing styling config with the project's data

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,13 @@ import { Providers } from "./providers";
 export default function RootLayout({ children }: { children: React.ReactNode; }) {
   return (
     <html lang="en">
-      <body className="light">
+       <head>
+        <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
+      </head>
+      <body className="light font-sans">
         <Providers>
-          <div className="flex flex-col h-screen">
+          <div className="flex flex-col h-screen bg-background">
             <main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
               {children}
             </main>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-import { nextui } from "@nextui-org/theme";
+const { nextui } = require("@nextui-org/react");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -10,11 +10,64 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["var(--font-sans)"],
-        mono: ["var(--font-geist-mono)"],
+        sans: ["Lato", "var(--font-sans)"],
+        mono: ["Roboto", "var(--font-geist-mono)"],
+      },
+      colors: {
+        primary: "#06433E",
+        secondary: "#0B051D",
+        red: "#F85252",
+        background: "#ECF2F1",
+        green: "#48937E",
+        darkblue1: "#14243C",
+        darkblue2: "#2A394E",
+        grey1: "#868686",
+        lightgreyborder: "#E8E8E8",
+        grey2: "#F0F0F0",
+      },
+      spacing: {
+        1: "8px",
+        2: "16px",
+        3: "24px",
+        4: "32px",
+        5: "40px",
+        6: "56px",
+        7: "72px",
+        8: "80px",
+        9: "96px",
+        10: "120px",
+      },
+      screens: {
+        md: "1440px",
+        sm: "768px",
+      },
+      fontSize: {
+        heading1: ["64px", { lineHeight: "80px" }],
+        heading2: ["48px", { lineHeight: "40px" }],
+        heading3: ["32px", { lineHeight: "35px" }],
+        heading4: ["24px", { lineHeight: "35px" }],
+        heading5: ["18px", { lineHeight: "20px" }],
+        buttonText: ["16px", { lineHeight: "auto" }],
+        paragraph1: ["16px", { lineHeight: "26px" }],
+        paragraph2: ["16px", { lineHeight: "26px" }],
       },
     },
   },
   darkMode: "class",
-  plugins: [nextui()],
+  plugins: [
+    nextui({
+      themes: {
+        light: {
+          layout: {},
+          colors: {
+            background: "#ECF2F1",
+          },
+          fontFamily: {
+            sans: ["Lato", "var(--font-sans)"],
+            mono: ["Roboto", "var(--font-geist-mono)"],
+          },
+        },
+      },
+    }),
+  ],
 };
