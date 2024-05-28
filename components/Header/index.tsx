bc5e47ac09454720ff643079b11219f2d5497d04
Edwin Andermyr <andermyredwin@gmail.com>
2024-05-28 18:48:51 +0200

    fixed the Link component issue as I was supposed to get the Link component from Next js and not next ui library here

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,4 +1,6 @@
-import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button} from "@nextui-org/react";
+'use client'
+import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Button} from "@nextui-org/react";
+import Link from "next/link";
 import MyFlags from "./Flags";
 import Image from "next/image";
 
@@ -10,17 +12,17 @@ export default function Header() {
       </NavbarBrand>
       <NavbarContent className="hidden sm:flex gap-4 justify-center">
         <NavbarItem isActive>
-          <Link  href="#" aria-current="page" className="text-white">
+          <Link  href="/" aria-current="page" className="text-white">
          Why Parking Time?
           </Link>
         </NavbarItem>
         <NavbarItem>
-          <Link className="text-white" href="#">
+          <Link className="text-white" href="/about">
             About us
           </Link>
         </NavbarItem>
         <NavbarItem>
-          <Link className="text-white" href="#">
+          <Link className="text-white" href="/news">
             News
           </Link>
         </NavbarItem>
@@ -32,7 +34,7 @@ export default function Header() {
       </NavbarContent>
       <NavbarContent className="justify-end">
         <NavbarItem>
-          <Button as={Link} href="#" className="bg-white text-black size-2 h-5">
+          <Button as={Link} href="/contact" className="bg-white text-black size-2 h-5">
             Contact us
           </Button>
         </NavbarItem>
