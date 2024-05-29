739b2430a01fc8c4753fdda26ddb0a908fe86d2f
Edwin Andermyr <andermyredwin@gmail.com>
2024-05-29 14:50:37 +0200

    fixed the font issue on nextui too

--- tailwind.config.js
+++ tailwind.config.js
@@ -63,8 +63,8 @@ module.exports = {
             background: "#ECF2F1",
           },
           fontFamily: {
-            sans: ["Lato", "var(--font-sans)"],
-            mono: ["Roboto", "var(--font-geist-mono)"],
+            sans: ["Lato", "sans-serif"],
+            mono: ["Roboto", "monospace"],
           },
         },
       },
