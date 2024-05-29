26a1c05b444fbd190e866faaa4d9751e4e5059a4
Memai2023 <maria.hong26@gmail.com>
2024-05-29 23:17:48 +0200

    added image, and text on the want to know more section

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
+import HomeKnow from "@/components/HomeKnow";
 
 export default function Home() {
   return (
     <section className="flex flex-col items-center justify-center">
-      <div className="text-center justify-center">
-        <h1 className="text-heading1"> Welcome to Parking Time!</h1>
-        <p className="text-paragraph1 font-mono">This is a paragraph</p>
-       </div> 
-       </section> 
+      <HomeKnow />
+    </section> 
   );
 }
