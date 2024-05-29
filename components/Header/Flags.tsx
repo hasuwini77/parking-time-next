8dbb76dad140125f9c4885b64522e7c34427b194
Edwin Andermyr <andermyredwin@gmail.com>
2024-05-29 13:58:16 +0200

    fixed the navbar for desktop and added a context language to it

--- components/Header/Flags.tsx
+++ components/Header/Flags.tsx
@@ -1,9 +1,16 @@
-'use client'
+"use client";  
 
 import React from "react";
-import {Dropdown, DropdownTrigger, DropdownMenu, DropdownItem, Avatar, User} from "@nextui-org/react";
+import { Dropdown, DropdownTrigger, DropdownMenu, DropdownItem, Avatar } from "@nextui-org/react";
+import { useLanguage } from "@/context/LanguageContext";
 
-export default function MyFlags() {
+const MyFlags: React.FC = () => {
+  const { language, setLanguage } = useLanguage();
+
+  const handleLanguageChange = (newLanguage: string) => {
+    setLanguage(newLanguage);
+  };
+
   return (
     <div className="flex items-center gap-4">
       <Dropdown placement="bottom-end">
@@ -14,18 +21,20 @@ export default function MyFlags() {
             className="transition-transform rounded-full h-[40px] w-[40px] bg-green"
             size="sm"
             src="/images/flags.png"
-            alt="swedish-flag"
+            alt="flag"
           />
         </DropdownTrigger>
-        <DropdownMenu aria-label="Profile Actions" variant="flat">
-          <DropdownItem key="swedish" className="h-14 gap-2">
+        <DropdownMenu aria-label="Language Selection" variant="flat">
+          <DropdownItem key="swedish" className="h-14 gap-2" onClick={() => handleLanguageChange("swedish")}>
             <p className="font-semibold">Swedish</p>
           </DropdownItem>
-          <DropdownItem key="english" className="h-14 gap-2">
+          <DropdownItem key="english" className="h-14 gap-2" onClick={() => handleLanguageChange("english")}>
             <p className="font-semibold">English</p>
           </DropdownItem>
         </DropdownMenu>
       </Dropdown>
     </div>
   );
-}
+};
+
+export default MyFlags;
