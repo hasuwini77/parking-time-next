1f4a080f6e9ff3a738ad0f8083350886aeade336
Edwin Andermyr <andermyredwin@gmail.com>
2024-05-30 21:55:07 +0200

    animated the logo list with fluidity

--- components/TrustedLogo/index.tsx
+++ components/TrustedLogo/index.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import Image from 'next/image';
-import styles from '.TrustedLogo.module.css'
 
 const TrustedLogo: React.FC = () => {
   return (
     <>
-<div className="w-full inline-flex flex-nowrap bg-white p-4">
-    <ul className="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none">
+<div className="w-full inline-flex flex-nowrap bg-white p-4 overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-200px),transparent_100%)">
+    <ul className="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll">
         <li>
             <Image src="/images/BrandedLogos/almi.svg" alt="almi" width={100} height={100}/>
         </li>
@@ -31,7 +30,35 @@ const TrustedLogo: React.FC = () => {
         <li>
         <Image src="/images/BrandedLogos/nass.svg" alt="almi" width={100} height={100}/>
         </li>
-    </ul>                
+    </ul>       
+
+    <ul className="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll aria-hidden={true">
+        <li>
+            <Image src="/images/BrandedLogos/almi.svg" alt="almi" width={100} height={100}/>
+        </li>
+        <li>
+        <Image src="/images/BrandedLogos/gobusiness.svg" alt="almi" width={100} height={100}/>
+        </li>
+        <li>
+        <Image src="/images/BrandedLogos/logo1.svg" alt="almi" width={100} height={100}/>
+        </li>
+        <li>
+        <Image src="/images/BrandedLogos/logo2.svg" alt="almi" width={100} height={100}/>
+        </li>
+        <li>
+        <Image src="/images/BrandedLogos/logo3.svg" alt="almi" width={100} height={100}/>
+        </li>
+        <li>
+        <Image src="/images/BrandedLogos/logo4.svg" alt="almi" width={100} height={100}/>
+        </li>
+        <li>
+        <Image src="/images/BrandedLogos/lulea.svg" alt="almi" width={100} height={100}/>
+        </li>
+        <li>
+        <Image src="/images/BrandedLogos/nass.svg" alt="almi" width={100} height={100}/>
+        </li>
+    </ul>       
+             
 </div>
     </>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,15 @@ module.exports = {
   ],
   theme: {
     extend: {
+      animation: {
+        "infinite-scroll": "infinite-scroll 25s linear infinite",
+      },
+      keyframes: {
+        "infinite-scroll": {
+          from: { transform: "translateX(0)" },
+          to: { transform: "translateX(-100%)" },
+        },
+      },
       fontFamily: {
         sans: ["Lato", "var(--font-sans)"],
         mono: ["Roboto", "var(--font-geist-mono)"],
