17c8ddb870775f7d5d6e8f9604725dccb3b5365c
Edwin Andermyr <andermyredwin@gmail.com>
2024-05-30 20:56:46 +0200

    added all logos into the component, styled it nicely and remove some default styling that was clashing with the banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
-
+'use client'
+import TrustedLogo from "@/components/TrustedLogo";
 export default function Home() {
   return (
+    <> 
     <section className="flex flex-col items-center justify-center">
       <div className="text-center justify-center">
         <h1 className="text-heading1"> Welcome to Parking Time!</h1>
         <p className="text-paragraph1 font-mono">This is a paragraph</p>
        </div> 
        </section> 
+       <TrustedLogo/>
+       </>
   );
 }
--- components/TrustedLogo/index.tsx
+++ components/TrustedLogo/index.tsx
@@ -1,31 +1,40 @@
 import React from 'react';
-import Slider from 'react-infinite-logo-slider';
+import Image from 'next/image';
+import styles from '.TrustedLogo.module.css'
 
 const TrustedLogo: React.FC = () => {
-    return (
-        <Slider
-            width="250px"
-            duration={40}
-            pauseOnHover={true}
-            blurBorders={false}
-            blurBorderColor={'#fff'} 
-        >
-            <Slider.Slide>
-                <img src="/slider/any.png" alt="any" className='w-36' />
-            </Slider.Slide>
-            <Slider.Slide>
-                <img src="/slider/any2.png" alt="any2" className='w-36' />
-            </Slider.Slide>
-            <Slider.Slide>
-                <img src="/slider/any3.png" alt="any3" className='w-36' />
-            </Slider.Slide>
-            <Slider.Slide>
-                <div>
-                    Other component...
-                </div>
-            </Slider.Slide>
-        </Slider>
-    );
+  return (
+    <>
+<div className="w-full inline-flex flex-nowrap bg-white p-4">
+    <ul className="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none">
+        <li>
+            <Image src="/images/BrandedLogos/almi.svg" alt="almi" width={100} height={100}/>
+        </li>
+        <li>
+        <Image src="/images/BrandedLogos/gobusiness.svg" alt="almi" width={100} height={100}/>
+        </li>
+        <li>
+        <Image src="/images/BrandedLogos/logo1.svg" alt="almi" width={100} height={100}/>
+        </li>
+        <li>
+        <Image src="/images/BrandedLogos/logo2.svg" alt="almi" width={100} height={100}/>
+        </li>
+        <li>
+        <Image src="/images/BrandedLogos/logo3.svg" alt="almi" width={100} height={100}/>
+        </li>
+        <li>
+        <Image src="/images/BrandedLogos/logo4.svg" alt="almi" width={100} height={100}/>
+        </li>
+        <li>
+        <Image src="/images/BrandedLogos/lulea.svg" alt="almi" width={100} height={100}/>
+        </li>
+        <li>
+        <Image src="/images/BrandedLogos/nass.svg" alt="almi" width={100} height={100}/>
+        </li>
+    </ul>                
+</div>
+    </>
+  );
 };
 
 export default TrustedLogo;
