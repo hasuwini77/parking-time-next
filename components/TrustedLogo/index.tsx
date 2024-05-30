5083222be021020981e6322426196e2a1b86ecfd
Edwin Andermyr <andermyredwin@gmail.com>
2024-05-30 17:17:33 +0200

    removed unneeded packages

--- components/TrustedLogo/index.tsx
+++ components/TrustedLogo/index.tsx
@@ -1,7 +1,31 @@
-export default function TrustedLogo () { 
-    return ( 
-        <div> 
-            this will be the Trusted logos 
-        </div>
-    )
-}
+import React from 'react';
+import Slider from 'react-infinite-logo-slider';
+
+const TrustedLogo: React.FC = () => {
+    return (
+        <Slider
+            width="250px"
+            duration={40}
+            pauseOnHover={true}
+            blurBorders={false}
+            blurBorderColor={'#fff'} 
+        >
+            <Slider.Slide>
+                <img src="/slider/any.png" alt="any" className='w-36' />
+            </Slider.Slide>
+            <Slider.Slide>
+                <img src="/slider/any2.png" alt="any2" className='w-36' />
+            </Slider.Slide>
+            <Slider.Slide>
+                <img src="/slider/any3.png" alt="any3" className='w-36' />
+            </Slider.Slide>
+            <Slider.Slide>
+                <div>
+                    Other component...
+                </div>
+            </Slider.Slide>
+        </Slider>
+    );
+};
+
+export default TrustedLogo;
