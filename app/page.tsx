b420aaecf47cf743a8b0369dc7f41cdf6277abc6
bedirgcmz <bdrgcmz@gmail.com>
2024-06-01 00:48:13 +0200

    Finished version 1

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import HomeHero from "@/components/HomeHero";
 import HomeHowWorks from "@/components/HomeHowWorks";
 import HomeStatistics from "@/components/HomeStatistics";
+import HomeWhyParking from "@/components/HomeWhyParking";
 
 export default function Home() {
   return (
     <section className="">
       <HomeHero />
+      <HomeWhyParking />
       <HomeHowWorks />
       <HomeStatistics />
     </section>
