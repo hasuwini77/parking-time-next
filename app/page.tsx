d42b1e36233016251e7ff6ee54705f312462d162
bedirgcmz <bdrgcmz@gmail.com>
2024-05-31 12:13:05 +0200

    Fixed mistake on page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import HomeHero from "@/components/HomeHero";
+import HomeHowWorks from "@/components/HomeHowWorks";
 import HomeStatistics from "@/components/HomeStatistics";
 
 export default function Home() {
   return (
     <section className="">
       <div className="text-center justify-center">
+        <HomeHero />
+        <HomeHowWorks />
         <HomeStatistics />
       </div>
     </section>
