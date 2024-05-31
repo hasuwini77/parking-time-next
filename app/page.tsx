c794c9ef9cc51528d2496e89533f66b7ed69afd4
Noah Gordon <noah.gordon@edu.cmeducations.se>
2024-05-31 15:47:14 +0200

    added max-width and mx-auto

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import HomeHero from "@/components/HomeHero";
 
 export default function Home() {
   return (
-    <section className="flex flex-col items-center justify-center">
+    <section className="">
       <HomeHero />
       <SmartParking />
     </section> 
