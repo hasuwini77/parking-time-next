903190920466d4542ab32e22044c81de9498778a
Edwin Andermyr <andermyredwin@gmail.com>
2024-06-01 11:28:22 +0200

    merged trusted logo into main

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
+import TrustedLogo from "@/components/TrustedLogo";
 import HomeHero from "@/components/HomeHero";
 import HomeHowWorks from "@/components/HomeHowWorks";
-import HomeStatistics from "@/components/HomeStatistics";
 
 export default function Home() {
   return (
     <section className="">
       <HomeHero />
       <HomeHowWorks />
-      <HomeStatistics />
+     <TrustedLogo/>
     </section>
   );
 }
