1b19b9ed5fa7cc4583ffb011b2f66a0b31bc55be
Memai2023 <maria.hong26@gmail.com>
2024-06-02 21:40:51 +0200

    adjusted content and added button

--- components/HomeKnow/index.tsx
+++ components/HomeKnow/index.tsx
@@ -4,50 +4,55 @@ import { motion } from "framer-motion";
 import { useLanguage } from "@/context/LanguageContext";
 import Image from "next/image";
 import matilda from "../../public/images/matilda.png";
+import ContactButton from "../ContactButton";
 import styles from './HomeKnow.module.css'
 
 const HomeKnow: React.FC = () => {
     const { language } = useLanguage();
 
     return (
-        <div className="bg-primary md:flex-col flex flex-row text-white ">
-            <div className={styles.mainContent}>
-                <div className={styles.leftContent}>
-                    <Image src={matilda} alt="A image of Matilda CEO"/>
-                    
-                    {language === "english" ? (
-                        <span className={styles.title}>
-                            Matilda Öhman, CEO <br/>
+        <div className={styles.container}>
+            <div className={styles.profile}>
+                <Image src={matilda} alt="A image of Matilda CEO"/>
+                
+                {language === "english" ? (
+                    <span className={styles.profilename}>
+                        Matilda Öhman, CEO <br/>
+                    </span>
+                ) : (
+                    <span className={styles.profilename}>
+                        Matilda Öhman, VD <br/>
+                    </span>
+                )}
+                    <span>Matilda@parkingtime.se</span>
+            </div>
+            <div className={styles.moreinfo}>
+                {language === "english" ? (
+                        <span>
+                            Want to know more? <br/>
                         </span>
                     ) : (
                         <span>
-                            Matilda Öhman, VD <br/>
+                            Vill du veta mer? <br/>
                         </span>
                     )}
-                        <span>Matilda@parkingtime.se</span>
-                </div>
-                <div>
-                    {language === "english" ? (
-                            <span>
-                                Want to know more? <br/>
-                            </span>
-                        ) : (
-                            <span>
-                                Vill du veta mer? <br/>
-                            </span>
-                        )}
 
-                    {language === "english" ? (
-                            <span>
-                                Contact us to find out more about our solution and how we can implement it in your municipality. <br/>
-                            </span>
-                        ) : (
-                            <span>
-                                Kontakta oss för att få veta mer om vår lösning och hur vi kan implementera det i din kommun.<br/>
-                            </span>
-                        )}
-
-                </div>
+                {language === "english" ? (
+                        <span>
+                            Contact us to find out more about our solution and <br/> how we can implement it in your municipality. <br/>
+                        </span>
+                    ) : (
+                        <span>
+                            Kontakta oss för att få veta mer om vår lösning och <br/> hur vi kan implementera det i din kommun.<br/>
+                        </span>
+                    )}
+                        <span className="mt-8">
+                            <ContactButton 
+                            linkHref="/contact" 
+                            SWtext="Kontakta oss" 
+                            ENtext="Contact us" 
+                        />
+                </span>
             </div>
         </div>
     );
