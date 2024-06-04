cbfbf9d5d9cd16a1f9818eef77ec6e73da5ffcb8
Memai2023 <maria.hong26@gmail.com>
2024-06-04 23:19:15 +0200

    Fixed indent, removed css

--- components/HomeKnow/index.tsx
+++ components/HomeKnow/index.tsx
@@ -4,7 +4,6 @@ import { useLanguage } from "@/context/LanguageContext";
 import Image from "next/image";
 import matilda from "../../public/images/matilda.png";
 import ContactButton from "../ContactButton";
-import styles from './HomeKnow.module.css'
 
 const HomeKnow: React.FC = () => {
     const { language } = useLanguage();
@@ -28,31 +27,31 @@ const HomeKnow: React.FC = () => {
             </div>
             <div className="w-full max-w-80 m-2.5 flex flex-col justify-center items-center p-1 text-center mb-12 md:items-start md:text-left md:pt-5 md:flex-1 md:max-w-none md:pb-12">
                 {language === "english" ? (
-                        <span className="text-4xl font-semibold pb-3 md:text-[44px] md:pt-4 md:pb-5 md:pr-3">
-                            Want to know more? <br/>
-                        </span>
-                    ) : (
-                        <span  className="text-4xl font-semibold pb-3 md:text-[44px]">
-                            Vill du veta mer? <br/>
-                        </span>
-                    )}
+                    <span className="text-4xl font-semibold pb-3 md:text-[44px] md:pt-4 md:pb-5 md:pr-3">
+                        Want to know more? <br/>
+                    </span>
+                ) : (
+                    <span  className="text-4xl font-semibold pb-3 md:text-[44px]">
+                        Vill du veta mer? <br/>
+                    </span>
+                )}
 
                 {language === "english" ? (
-                        <span className="text-base pb-1 md:max-w-[400px]">
-                            Contact us to find out more about our solution and how we can implement it in your municipality. <br/>
-                        </span>
-                    ) : (
-                        <span className="text-base pb-1">
-                            Kontakta oss för att få veta mer om vår lösning och hur vi kan implementera det i din kommun.
-                        </span>
-                    )}
-                        <span className="w-full sm:max-w-[460px] text-center pt-3 md:text-left">
-                            <ContactButton 
-                            linkHref="/contact" 
-                            SWtext="Kontakta oss" 
-                            ENtext="Contact us"
+                    <span className="text-base pb-1 md:max-w-[400px]">
+                        Contact us to find out more about our solution and how we can implement it in your municipality. <br/>
+                    </span>
+                ) : (
+                    <span className="text-base pb-1">
+                        Kontakta oss för att få veta mer om vår lösning och hur vi kan implementera det i din kommun.
+                    </span>
+                )}
+                    <span className="w-full sm:max-w-[460px] text-center pt-3 md:text-left">
+                        <ContactButton 
+                        linkHref="/contact" 
+                        SWtext="Kontakta oss" 
+                        ENtext="Contact us"
                         />
-                        </span>
+                    </span>
             </div>
         </div>
     );
