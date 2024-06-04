52710038e5a561964397aaf550251b7d32cfca1e
Memai2023 <maria.hong26@gmail.com>
2024-06-05 00:19:34 +0200

    Final adjustments + Tailwind

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -1,8 +1,10 @@
 import NewsHero from "@/components/NewsHero";
+import TrustedLogo from "@/components/TrustedLogo";
 export default function NewsPage() {
     return (
       <div>
         <NewsHero/>
+        <TrustedLogo/>
       </div>
     );
   }
--- components/NewsHero/index.tsx
+++ components/NewsHero/index.tsx
@@ -1,13 +1,10 @@
-import styles from './NewsHero.module.css';
-
 const NewsHero = () => {
     return (
-        <div className={styles.NewsHero}>
-            <div className={styles.overlay}>
-                <ul>
-                    <li><span>News</span></li>
-                </ul>
-                <span className={styles.title}>
+        <div className="w-full h-[50vh] relative bg-cover bg-[50%_50%] bg-[url('/images/newsHero.png')] flex justify-start items-start overflow-hidden">
+            <div className="w-full md:w-2/5 h-full bg-[rgba(0,0,0,0.2)] backdrop-blur-[2px] md:backdrop-blur-[10px] p-5 md:p-7 flex flex-col justify-center items-start text-white">
+                <span className="list-disc text-xl list-inside pl-0 ml-0">
+                    <span className="text-red text-[27px]">&bull;</span> News</span>
+                <span className="text-5xl font-[530] leading-tight pt-3">
                 We are Tranforming <br/>
                 Parking Experiences
                 </span>
