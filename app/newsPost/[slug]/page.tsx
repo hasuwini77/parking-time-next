1aabda633c3177408a565eef7da6d2b2369c21ed
Edwin Andermyr <andermyredwin@gmail.com>
2024-06-05 16:12:56 +0200

    added basic responsive styling to ourNews component

--- app/newsPost/[slug]/page.tsx
+++ app/newsPost/[slug]/page.tsx
@@ -51,8 +51,6 @@ const NewsDetail = ({ params }: { params: { slug: string } }) => {
       {post ? (
         <>
           <h1>{post.title}</h1>
-          <p>{post.body}</p>
-          {/* Other fields can be added here */}
         </>
       ) : (
         <p>Post not found</p>
