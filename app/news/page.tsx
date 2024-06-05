46326a30c7da7ef6c2f4cd6c079b06a94be56bfd
Memai2023 <maria.hong26@gmail.com>
2024-06-05 12:40:38 +0200

    added swedish text and resolve the hyphen prob

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -1,10 +1,8 @@
 import NewsHero from "@/components/NewsHero";
-import TrustedLogo from "@/components/TrustedLogo";
 export default function NewsPage() {
     return (
       <div>
         <NewsHero/>
-        <TrustedLogo/>
       </div>
     );
   }
