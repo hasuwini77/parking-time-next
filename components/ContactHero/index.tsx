1faca6215314fa8627a0b7befe2b44b1d561c47e
Noah Gordon <noah.gordon@edu.cmeducations.se>
2024-06-07 16:40:37 +0200

    fixed lg-width

--- components/ContactHero/index.tsx
+++ components/ContactHero/index.tsx
@@ -5,7 +5,7 @@ const ContactHero: React.FC = () => {
     const { language } = useLanguage();
     return (
         <div className="w-full h-[50vh] relative bg-cover bg-[50%_50%] bg-[url('/images/contactHero.png')] flex justify-start items-start overflow-hidden">
-            <div className="w-full md:w-[44%] h-full bg-[rgba(0,0,0,0.2)] backdrop-blur-[2px] md:backdrop-blur-[10px] p-5 flex flex-col justify-center items-start text-white">
+            <div className="w-full md:w-[44%] lg:w-2/5 h-full bg-[rgba(0,0,0,0.2)] backdrop-blur-[2px] md:backdrop-blur-[10px] p-5 flex flex-col justify-center items-start text-white">
                 <span className="list-disc text-xl list-inside pl-0 ml-0">
                     <span className="text-red text-[27px]">&bull;&nbsp;</span>
                     {language === "english" ? (
