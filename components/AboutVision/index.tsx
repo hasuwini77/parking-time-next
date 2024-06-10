0d92b2a73f4f8ff6ddaad56656c37b3be335b9bb
Memai2023 <maria.hong26@gmail.com>
2024-06-10 14:47:01 +0200

    building about vision structure

--- components/AboutVision/index.tsx
+++ components/AboutVision/index.tsx
@@ -1,14 +1,13 @@
 'use client';
 import React from 'react';
-import styles from '/AboutVision.module.css'
 
 const AboutVision = () => {
     return (
-        <div className={styles.visionContainer}>
-            <div className={styles.textContainer}>
+        <div>
+            <div>
 
             </div>
-            <div className={styles.imageContainer}>
+            <div>
 
             </div>
         </div>
