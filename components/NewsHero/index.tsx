42991a8bf7a94876d7b03f4bf3e079ac604827bc
Memai2023 <maria.hong26@gmail.com>
2024-06-10 22:44:28 +0200

    modified for responsivness

--- components/NewsHero/index.tsx
+++ components/NewsHero/index.tsx
@@ -4,8 +4,8 @@ import { useLanguage } from "@/context/LanguageContext"
 const NewsHero: React.FC = () => {
     const { language } = useLanguage();
     return (
-        <div className="w-full h-[50vh] md:min-h-[350px] md:h-[35vh] lg:h-[50vh] relative bg-cover bg-[50%_50%] bg-[url('/images/newsHero.png')] flex justify-start items-start overflow-hidden">
-            <div className="w-full md:min-w-[350px] md:w-2/5 h-full bg-[rgba(0,0,0,0.2)] backdrop-blur-[2px] md:backdrop-blur-[10px] p-5 flex flex-col justify-center items-start text-white">
+        <div className="w-full min-h-[50vh] relative bg-cover bg-[50%_50%] bg-[url('/images/newsHero.png')] flex justify-start items-start overflow-hidden">
+            <div className="w-full md:min-w-[350px] md:w-2/5 min-h-[50vh] bg-[rgba(0,0,0,0.2)] backdrop-blur-[2px] md:backdrop-blur-[10px] p-5 flex flex-col justify-center items-start text-white">
                 <span className="list-disc text-xl list-inside pl-0 ml-0">
                     <span className="text-red text-[27px]">&bull;&nbsp;</span>
                     {language === "english" ? (
