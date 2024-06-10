6a6296857e2d63b8196e713f5102a2aa92a0ca4c
Edwin Andermyr <andermyredwin@gmail.com>
2024-06-10 14:28:06 +0200

    setting up the AboutTeam Page

---
+++ components/AboutTeam/index.tsx
@@ -0,0 +1,12 @@
+import { useLanguage } from "@/context/LanguageContext";
+
+const AboutTeam = () => {
+  const { language } = useLanguage();
+  return (
+    <div className="w-full py-32 md:py-44 px-4 md:px-20 flex flex-col justify-between items-center">
+      <h1> </h1>
+      <p> </p>
+      <div className="flex flex-row"></div>
+    </div>
+  );
+};
