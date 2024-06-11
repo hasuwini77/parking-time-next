efe1ae9196d544a98b6f9cbc1324fe67c9bda571
Edwin Andermyr <andermyredwin@gmail.com>
2024-06-11 11:21:31 +0200

    added a new frame to the flags and an animation framer motion to it

--- components/Header/Flags.tsx
+++ components/Header/Flags.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { useState } from "react";
+import { motion } from "framer-motion";
 import React from "react";
 import {
   Dropdown,
@@ -14,20 +16,29 @@ import Image from "next/image";
 
 const MyFlags: React.FC = () => {
   const { language, setLanguage } = useLanguage();
+  const [clicked, setClicked] = useState(false);
 
   const handleLanguageChange = (newLanguage: string) => {
     setLanguage(newLanguage);
+    setClicked(true);
+    setTimeout(() => setClicked(false), 300);
   };
 
   return (
     <div className="flex items-center gap-4">
       <Dropdown placement="bottom-end">
         <DropdownTrigger>
-          <Image
-            className="transition-transform rounded-full h-[40px] w-[40px] bg-green border"
-            src={language === "english" ? ukflag : sweflag}
-            alt="flag"
-          />
+          <motion.div
+            whileTap={{ scale: 1.1 }}
+            animate={clicked ? { scale: 1.1 } : { scale: 1 }}
+            transition={{ type: "spring", stiffness: 500, damping: 30 }}
+          >
+            <Image
+              className="transition-transform rounded-full h-[40px] w-[40px] bg-green p-[2px] shadow-md"
+              src={language === "english" ? ukflag : sweflag}
+              alt="flag"
+            />
+          </motion.div>
         </DropdownTrigger>
         <DropdownMenu
           aria-label="Language Selection"
