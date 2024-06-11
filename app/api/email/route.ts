3e3edff55274571a880aece39ae2b64985bf1c0f
Noah Gordon <noah.gordon@edu.cmeducations.se>
2024-06-11 22:35:58 +0200

    set up gmail

--- app/api/email/route.ts
+++ app/api/email/route.ts
@@ -1,5 +1,84 @@
-import { type NextRequest, NextResponse } from 'next/server';
+/* import { type NextRequest, NextResponse } from 'next/server';
 
 export async function POST(request: NextRequest) {
   return NextResponse.json('Hello from API!');
+} */
+
+
+/* import { NextResponse } from "next/server";
+import nodemailer from "nodemailer";
+
+const user = process.env.EMAIL;
+const pass = process.env.PASSWORD;
+
+export async function POST(request: Request) {
+  try {
+    const { name, email, message } = await request.json();
+    
+    const transporter = nodemailer.createTransport({
+      service: "zoho",
+      host: "smtpro.zoho.in",
+      port: 465
+      secure: true,
+      auth: {
+        user,
+        pass,
+      },
+    });
+
+    const mailOptions = {
+      from: "user",
+      to: "the-email-you-want-to-receive-the-message",
+      subject: "New message from your-website",
+      text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
+    };
+
+    await transporter.sendMail(mailOptions);
+
+    return NextResponse.json(
+      { message: "Message sent successfully" },
+      { status: 200 },
+    );
+  } catch (error) {
+    return new NextResponse("Failed to send message.", { status: 500 })
+  }
+} */
+
+
+
+import { NextResponse } from "next/server";
+import nodemailer from "nodemailer";
+
+const user = process.env.EMAIL;
+const pass = process.env.PASSWORD;
+
+export async function POST(request: Request) {
+  try {
+    const { name, jobtitle, email, phone, reason, message } = await request.json();
+
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user,
+        pass,
+      },
+    });
+
+    const mailOptions = {
+      from: user,
+      to: "test.smartparking12@gmail.com",
+      subject: "New message from your website",
+      text: `Name: ${name}\nJob Title: ${jobtitle}\nEmail: ${email}\nPhone: ${phone}\nReason: ${reason}\nMessage: ${message}`,
+    };
+
+    await transporter.sendMail(mailOptions);
+
+    return NextResponse.json(
+      { message: "Message sent successfully" },
+      { status: 200 },
+    );
+  } catch (error) {
+    console.error('Error sending email:', error);
+    return new NextResponse("Failed to send message.", { status: 500 });
+  }
 }
--- components/ContactForm/index.tsx
+++ components/ContactForm/index.tsx
@@ -1,4 +1,4 @@
-'use client';
+/* 'use client';
 
 import { FC } from 'react';
 import { useForm } from 'react-hook-form';
@@ -155,3 +155,169 @@ const ContactForm: FC = () => {
 
 export default ContactForm;
 
+ */
+
+
+
+'use client';
+
+import { FC } from 'react';
+import { useForm } from 'react-hook-form';
+import { FaEnvelope, FaPhone, FaLocationDot } from "react-icons/fa6";
+import SendEmailButton from "../SendEmailButton";
+import axios from 'axios';
+import toast from 'react-hot-toast';
+import styles from './ContactForm.module.css';
+
+export type FormData = {
+  name: string;
+  jobtitle: string;
+  email: string;
+  phone: number;
+  reason: string;
+  message: string;
+  terms: boolean;
+};
+
+const ContactForm: FC = () => {
+  const { register, handleSubmit, formState: { errors }, reset } = useForm<FormData>();
+
+  async function onSubmit(data: FormData) {
+    try {
+      await axios.post("/api/email", data);
+      reset();
+      toast.success("Message received. We will contact you as soon as possible.");
+    } catch (error) {
+      toast.error("Failed to send message. Please try again.");
+    }
+  }
+
+  return (
+    <section className="xl:gap-10 md:py-7 xl:max-w-7xl lg:max-w-3xl md:max-w-2xl mx-auto flex flex-col xl:flex-row items-center pt-5 pb-4">
+      <div className="p-4 self-start">
+        <h1 className="font-bold text-5xl md:text-heading2 mb-3">Contact Us</h1>
+        <p className="mb-4 font-mono text-base leading-8 font-[500]">
+          Whether you have a question, feedback, or need assistance, please fill out the form below, and our team will get back to you as soon as possible.
+        </p>
+        <ul className="list-none">
+          <li className="mb-2 flex items-center">
+            <FaEnvelope className="mr-2" />
+            <a href="mailto:info@parkingtime.se" className="text-blue-600 hover:underline">info@parkingtime.se</a>
+          </li>
+          <li className="mb-2 flex items-center">
+            <FaPhone className="mr-2" />
+            <a href="tel:+46723991515" className="text-blue-600 hover:underline">+46 72 399 15 15</a>
+          </li>
+          <li className="mb-2 flex items-center">
+            <FaLocationDot className="mr-2" />
+            <a href="https://maps.google.com/?q=Nyköpingsvägen 52, 611 50 NYKÖPING" target="_blank" rel="noopener noreferrer" className="text-blue-600 hover:underline">
+              Nyköpingsvägen 52 | 611 50 NYKÖPING
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div className="container mx-auto p-4 rounded-lg">
+        <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+          <div>
+            <label htmlFor="name" className="block text-sm font-medium text-gray-700">
+              First and last name
+            </label>
+            <input
+              type="text"
+              id="name"
+              placeholder="First and last name"
+              className="mt-1 p-2 block w-full border border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring-blue-500"
+              {...register('name', { required: true })}
+            />
+            {errors.name && <span className="text-red-500 text-sm">This field is required</span>}
+          </div>
+          <div>
+            <label htmlFor="job" className="block text-sm font-medium text-gray-700">
+              Job title
+            </label>
+            <input
+              type="text"
+              id="job"
+              placeholder="Job title"
+              className="mt-1 p-2 block w-full border border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring-blue-500"
+              {...register('jobtitle', { required: true })}
+            />
+            {errors.jobtitle && <span className="text-red-500 text-sm">This field is required</span>}
+          </div>
+          <div>
+            <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              placeholder="ex. name@email.com"
+              className="mt-1 p-2 block w-full border border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring-blue-500"
+              {...register('email', { required: true })}
+            />
+            {errors.email && <span className="text-red-500 text-sm">This field is required</span>}
+          </div>
+          <div>
+            <label htmlFor="phone" className="block text-sm font-medium text-gray-700">
+              Phone number
+            </label>
+            <input
+              type="number"
+              id="phone"
+              placeholder="+46708123456"
+              className="mt-1 p-2 block w-full border border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring-blue-500"
+              {...register('phone', { required: true })}
+            />
+            {errors.phone && <span className="text-red-500 text-sm">This field is required</span>}
+          </div>
+          <div>
+            <label htmlFor="reason" className="block text-sm font-medium text-gray-700">
+              Reason for contact
+            </label>
+            <select
+              id="reason"
+              className="mt-1 p-2 block w-full border border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring-blue-500"
+              {...register('reason', { required: true })}
+            >
+              <option value="option1">Option1</option>
+              <option value="option2">Option2</option>
+              <option value="option3">Option3</option>
+              <option value="option4">Option4</option>
+            </select>
+            {errors.reason && <span className="text-red-500 text-sm">This field is required</span>}
+          </div>
+          <div>
+            <label htmlFor="message" className="block text-sm font-medium text-gray-700">
+              Write us a message
+            </label>
+            <textarea
+              id="message"
+              rows={4}
+              placeholder="Type your message..."
+              className="mt-1 p-2 block w-full border border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring-blue-500 h-48 resize-y"
+              {...register('message', { required: true })}
+            ></textarea>
+            {errors.message && <span className="text-red-500 text-sm">This field is required</span>}
+          </div>
+          <div className="flex items-center">
+            <input
+              type="checkbox"
+              id="terms"
+              className="mr-2"
+              {...register('terms', { required: true })}
+            />
+            <label htmlFor="terms" className="mr-2 text-sm font-medium text-gray-700">
+              I accept the terms 
+            </label>
+            {errors.terms && <span className="text-red-500 text-sm">You must accept the terms!</span>}
+          </div>
+          <div>
+            <SendEmailButton SWtext="Skicka meddelande" ENtext="Send message" />
+          </div>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default ContactForm;
