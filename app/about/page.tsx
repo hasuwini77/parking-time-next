2b4bc8dd92e9c17cab4b515869a3e2598bc6fd23
Edwin Andermyr <andermyredwin@gmail.com>
2024-06-13 13:04:55 +0200

    setting up the component with a button to contact

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,12 +2,14 @@ import AboutHero from "@/components/AboutHero";
 import AboutVision from "@/components/AboutVision";
 import Quotes from "@/components/AboutQuotes";
 import AboutKnow from "@/components/AboutKnow";
+import AboutCreativeScroll from "@/components/AboutCreativeScroll";
 
 export default function AboutPage() {
   return (
     <div>
       <AboutHero />
-      <AboutVision/>
+      <AboutVision />
+      <AboutCreativeScroll />
       <Quotes />
       <AboutKnow />
     </div>
--- components/AboutCreativeScroll/index.tsx
+++ components/AboutCreativeScroll/index.tsx
@@ -1,5 +1,32 @@
-const AboutCreativeScroll = () => {
-  return <div></div>;
+"use client";
+import { useLanguage } from "@/context/LanguageContext";
+import ContactButton from "../ContactButton";
+
+const AboutCreativeScroll: React.FC = () => {
+  const { language } = useLanguage();
+  return (
+    <div className="aboutScroll py-4 h-[40vh] flex flex-col items-center justify-center w-full">
+      <h3 className="text-bold text-3xl">
+        {language === "english" ? "Wanna join us?" : "Vill du vara med?"}
+      </h3>
+      <p className="max-w-[40%] text-center p-2 text-xl">
+        {" "}
+        {language === "english"
+          ? "We are always eager to welcome new members to our team at Parking Time. If you are interested in joining us, please fill out our open application form. We look forward to hearing from you!"
+          : "Vi är alltid ivriga att välkomna nya medlemmar till vårt team på Parking Time. Om du är intresserad av att gå med oss, vänligen fyll i vårt öppna ansökningsformulär. Vi ser fram emot att höra från dig!"}
+      </p>
+      <p className="py-4 text-lg">
+        {language === "english"
+          ? "Fill in the form below"
+          : "Fyll i formuläret nedan"}
+      </p>
+      <ContactButton
+        linkHref="/contact"
+        SWtext="Kontakta oss"
+        ENtext="Contact us"
+      />
+    </div>
+  );
 };
 
 export default AboutCreativeScroll;
