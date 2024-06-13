455197a7c06d06edaa0a16ac4ccd207dccdce134
Noah Gordon <noah.gordon@edu.cmeducations.se>
2024-06-13 20:50:00 +0200

    fixed sendemailbutton

--- components/ContactForm/index.tsx
+++ components/ContactForm/index.tsx
@@ -6,6 +6,7 @@ import { FaEnvelope, FaPhone, FaLocationDot } from "react-icons/fa6";
 import SendEmailButton from "../SendEmailButton";
 import axios from 'axios';
 import toast from 'react-hot-toast';
+import { useLanguage } from '@/context/LanguageContext';
 
 export type FormData = {
   name: string;
@@ -18,24 +19,27 @@ export type FormData = {
 };
 
 const ContactForm: FC = () => {
+  const { language } = useLanguage();
   const { register, handleSubmit, formState: { errors }, reset } = useForm<FormData>();
 
   async function onSubmit(data: FormData) {
     try {
       await axios.post("/api/email", data);
       reset();
-      toast.success("Message received. We will contact you as soon as possible.");
+      toast.success(language === "english" ? "Message received. We will contact you as soon as possible." : "Meddelandet mottaget. Vi kommer att kontakta dig så snart som möjligt.");
     } catch (error) {
-      toast.error("Failed to send message. Please try again.");
+      toast.error(language === "english" ? "Failed to send message. Please try again." : "Misslyckades med att skicka meddelande. Försök igen.");
     }
   }
 
   return (
     <section className="xl:gap-10 md:py-7 xl:max-w-7xl lg:max-w-3xl md:max-w-2xl mx-auto flex flex-col xl:flex-row items-center pt-5 pb-4">
-      <div className="p-4 self-start">
-        <h1 className="font-bold text-5xl md:text-heading2 mb-3">Contact Us</h1>
+      <div className="p-4 xl:p-5 self-start">
+        <h1 className="font-bold text-5xl md:text-heading2 mb-3">
+          {language === "english" ? "Contact Us" : "Kontakta Oss"}
+        </h1>
         <p className="mb-4 font-mono text-base leading-8 font-[500]">
-          Whether you have a question, feedback, or need assistance, please fill out the form below, and our team will get back to you as soon as possible.
+          {language === "english" ? "Whether you have a question, feedback, or need assistance, please fill out the form below, and our team will get back to you as soon as possible." : "Om du har en fråga, feedback eller behöver hjälp, fyll i formuläret nedan så kommer vårt team att återkomma till dig så snart som möjligt."}
         </p>
         <ul className="list-none">
           <li className="mb-2 flex items-center">
@@ -58,36 +62,36 @@ const ContactForm: FC = () => {
         <form onSubmit={handleSubmit(onSubmit)} className="space-y-3">
           <div>
             <label htmlFor="name" className="block text-sm font-medium text-gray-700">
-              First and last name <span className='text-red text-bold text-md'>*</span>
+              {language === "english" ? "First and last name" : "För- och efternamn"} <span className='text-red text-bold text-md'>*</span>
             </label>
             <input
               type="text"
               id="name"
-              placeholder="First and last name"
+              placeholder={language === "english" ? "First and last name" : "För- och efternamn"}
               className="mt-1 p-2 block w-full border border-gray-300 rounded-[10px] shadow-sm focus:border-blue-500 focus:ring-blue-500"
               {...register('name', { required: true })}
             />
-            {errors.name && <span className="text-red text-sm">This field is required</span>}
+            {errors.name && <span className="text-red text-sm">{language === "english" ? "This field is required" : "Detta fält är obligatoriskt"}</span>}
           </div>
           <div>
             <label htmlFor="job" className="block text-sm font-medium text-gray-700">
-              Job title <span className='ps-[4px] text-[12px] text-grey1'>(optional)</span>
+              {language === "english" ? "Job title" : "Jobbtitel"} <span className='ps-[4px] text-[12px] text-grey1'>{language === "english" ? "(optional)" : "(valfritt)"}</span>
             </label>
             <input
               type="text"
               id="job"
-              placeholder="Job title"
+              placeholder={language === "english" ? "Job title" : "Jobbtitel"}
               className="mt-1 p-2 block w-full border border-gray-300 rounded-[10px] shadow-sm focus:border-blue-500 focus:ring-blue-500"
               {...register('jobtitle', { required: false })}
             />
-            {errors.jobtitle && <span className="text-red text-sm">This field is required</span>}
+            {errors.jobtitle && <span className="text-red text-sm">{language === "english" ? "This field is required" : "Detta fält är obligatoriskt"}</span>}
           </div>
 
           {/* email & phone on the same line, min-width 1280px */}
           <div className='hidden xl:flex'>
             <div className='mr-2 lg:w-8/12'>
               <label htmlFor="email" className="block text-sm font-medium text-gray-700">
-                Email <span className='text-red text-bold text-md'>*</span>
+                {language === "english" ? "Email" : "E-post"} <span className='text-red text-bold text-md'>*</span>
               </label>
               <input
                 type="email"
@@ -96,11 +100,11 @@ const ContactForm: FC = () => {
                 className="mt-1 p-2 block w-full border border-gray-300 rounded-[10px] shadow-sm focus:border-blue-500 focus:ring-blue-500"
                 {...register('email', { required: true })}
               />
-              {errors.email && <span className="text-red text-sm">This field is required</span>}
+              {errors.email && <span className="text-red text-sm">{language === "english" ? "This field is required" : "Detta fält är obligatoriskt"}</span>}
             </div>
             <div className='lg:w-4/12'>
               <label htmlFor="phone" className="block text-sm font-medium text-gray-700">
-                Phone number <span className='ps-[4px] text-[12px] text-grey1'>(optional)</span>
+                {language === "english" ? "Phone number" : "Telefonnummer"} <span className='ps-[4px] text-[12px] text-grey1'>{language === "english" ? "(optional)" : "(valfritt)"}</span>
               </label>
               <input
                 type="number"
@@ -109,62 +113,62 @@ const ContactForm: FC = () => {
                 className="mt-1 p-2 block w-full border border-gray-300 rounded-[10px] shadow-sm focus:border-blue-500 focus:ring-blue-500"
                 {...register('phone', { required: false })}
               />
-              {errors.phone && <span className="text-red text-sm">This field is required</span>}
+              {errors.phone && <span className="text-red text-sm">{language === "english" ? "This field is required" : "Detta fält är obligatoriskt"}</span>}
             </div>
           </div>
 
-            <div className='xl:mr-2 xl:w-8/12 xl:hidden'>
-              <label htmlFor="email" className="block text-sm font-medium text-gray-700">
-                Email <span className='text-red text-bold text-md'>*</span>
-              </label>
-              <input
-                type="email"
-                id="email"
-                placeholder="ex. name@email.com"
-                className="mt-1 p-2 block w-full border border-gray-300 rounded-[10px] shadow-sm focus:border-blue-500 focus:ring-blue-500"
-                {...register('email', { required: true })}
-              />
-              {errors.email && <span className="text-red text-sm">This field is required</span>}
-            </div>
-            <div className='xl:w-4/12 xl:hidden'>
-              <label htmlFor="phone" className="block text-sm font-medium text-gray-700">
-                Phone number <span className='ps-[4px] text-[12px] text-grey1'>(optional)</span>
-              </label>
-              <input
-                type="number"
-                id="phone"
-                placeholder="+46708123456"
-                className="mt-1 p-2 block w-full border border-gray-300 rounded-[10px] shadow-sm focus:border-blue-500 focus:ring-blue-500"
-                {...register('phone', { required: false })}
-              />
-              {errors.phone && <span className="text-red text-sm">This field is required</span>}
-            </div>
+          <div className='xl:mr-2 xl:w-8/12 xl:hidden'>
+            <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+              {language === "english" ? "Email" : "E-post"} <span className='text-red text-bold text-md'>*</span>
+            </label>
+            <input
+              type="email"
+              id="email"
+              placeholder="ex. name@email.com"
+              className="mt-1 p-2 block w-full border border-gray-300 rounded-[10px] shadow-sm focus:border-blue-500 focus:ring-blue-500"
+              {...register('email', { required: true })}
+            />
+            {errors.email && <span className="text-red text-sm">{language === "english" ? "This field is required" : "Detta fält är obligatoriskt"}</span>}
+          </div>
+          <div className='xl:w-4/12 xl:hidden'>
+            <label htmlFor="phone" className="block text-sm font-medium text-gray-700">
+              {language === "english" ? "Phone number" : "Telefonnummer"} <span className='ps-[4px] text-[12px] text-grey1'>{language === "english" ? "(optional)" : "(valfritt)"}</span>
+            </label>
+            <input
+              type="number"
+              id="phone"
+              placeholder="+46708123456"
+              className="mt-1 p-2 block w-full border border-gray-300 rounded-[10px] shadow-sm focus:border-blue-500 focus:ring-blue-500"
+              {...register('phone', { required: false })}
+            />
+            {errors.phone && <span className="text-red text-sm">{language === "english" ? "This field is required" : "Detta fält är obligatoriskt"}</span>}
+          </div>
 
           <div>
             <label htmlFor="reason" className="block text-sm font-medium text-gray-700">
-              Reason for contact <span className='ps-[4px] text-[12px] text-grey1'>(optional)</span>
+              {language === "english" ? "Reason for contact" : "Kontaktorsak"} <span className='ps-[4px] text-[12px] text-grey1'>{language === "english" ? "(optional)" : "(valfritt)"}</span>
             </label>
             <input
               type="text"
               id="reason"
-              placeholder="Reason for contact"
+              placeholder={language === "english" ? "Reason for contact" : "Kontaktorsak"}
               className="mt-1 p-2 block w-full border border-gray-300 rounded-[10px] shadow-sm focus:border-blue-500 focus:ring-blue-500"
               {...register('reason', { required: false })}
             />
-            {errors.reason && <span className="text-red text-sm">This field is required</span>}
+            {errors.reason && <span className="text-red text-sm">{language === "english" ? "This field is required" : "Detta fält är obligatoriskt"}</span>}
           </div>
           <div>
             <label htmlFor="message" className="block text-sm font-medium text-gray-700">
-              Write us a message <span className='text-red text-bold text-md'>*</span>
+              {language === "english" ? "Write us a message" : "Skriv ett meddelande"} <span className='text-red text-bold text-md'>*</span>
             </label>
             <textarea
               id="message"
               rows={4}
-              placeholder="Type your message..."
+              placeholder={language === "english" ? "Type your message..." : "Skriv ditt meddelande..."}
               className="mt-1 p-2 block w-full border border-gray-300 rounded-[10px] shadow-sm focus:border-blue-500 focus:ring-blue-500 h-48 resize-y"
               {...register('message', { required: true })}
             ></textarea>
-            {errors.message && <span className="text-red text-sm">This field is required</span>}
+            {errors.message && <span className="text-red text-sm">{language === "english" ? "This field is required" : "Detta fält är obligatoriskt"}</span>}
           </div>
           <div className="flex items-center !mt-[10px]">
             <input
@@ -174,9 +178,9 @@ const ContactForm: FC = () => {
               {...register('terms', { required: true })}
             />
             <label htmlFor="terms" className="mr-2 text-sm font-medium text-gray-700">
-              I accept the terms 
+              {language === "english" ? "I accept the terms" : "Jag accepterar villkoren"}
             </label>
-            {errors.terms && <span className="text-red text-sm">You must accept the terms!</span>}
+            {errors.terms && <span className="text-red text-sm">{language === "english" ? "You must accept the terms!" : "Du måste acceptera villkoren!"}</span>}
           </div>
           <div>
             <SendEmailButton SWtext="Skicka meddelande" ENtext="Send message" />
--- components/SendEmailButton/index.tsx
+++ components/SendEmailButton/index.tsx
@@ -1,38 +1,3 @@
-/* import { motion } from "framer-motion";
-import { useLanguage } from "@/context/LanguageContext";
-
-interface DownloadProps {
-  SWtext: string;
-  ENtext: string;
-  type?: "button" | "submit" | "reset";
-}
-
-const DownloadButton: React.FC<DownloadProps> = ({ SWtext, ENtext, type = "button" }) => {
-  const { language } = useLanguage();
-
-  return (
-    <motion.button
-      type={type}
-      className="rounded-3xl md:w-48 w-full px-5 py-2 md:py-2 bg-black text-white custom-hover-home-hero-download-button"
-      variants={{
-        hidden: { opacity: 0, scale: 0.5 },
-        visible: {
-          opacity: 1,
-          scale: 1,
-          transition: { type: "spring", delay: 0.3, duration: 1.3 },
-        },
-      }}
-      initial="hidden"
-      animate="visible"
-    >
-      {language === "english" ? ENtext : SWtext}
-    </motion.button>
-  );
-};
-
-export default DownloadButton;
-*/
-
 import { motion } from "framer-motion";
 import { useLanguage } from "@/context/LanguageContext";
 
@@ -47,7 +12,7 @@ const SendEmailButton: React.FC<SendEmailButtonProps> = ({ SWtext, ENtext }) => 
   return (
     <motion.button
       type="submit"
-      className="rounded-3xl md:w-48 w-full px-5 py-2 md:py-2 bg-black text-white custom-hover-home-hero-download-button"
+      className="rounded-3xl md:w-48 w-full px-3 py-2 md:py-2 bg-black text-white custom-hover-home-hero-download-button"
       variants={{
         hidden: { opacity: 0, scale: 0.5 },
         visible: {
