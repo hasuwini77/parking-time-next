10b6b07d94de3cce53018b2377c1facf879e6fc7
Wilma NOrman <wilmanorman999@gmail.com>
2024-06-14 10:09:57 +0200

    fixing error

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,29 +1,22 @@
 import AboutHero from "@/components/AboutHero";
 import AboutVision from "@/components/AboutVision";
 import Quotes from "@/components/AboutQuotes";
-import AboutTeam from "@/components/AboutTeam";
 import AboutKnow from "@/components/AboutKnow";
-<<<<<<< HEAD
 import AboutCreativeScroll from "@/components/AboutCreativeScroll";
-=======
 import AboutTeam from "@/components/AboutTeam";
->>>>>>> feature-25-contact-form
 
 export default function AboutPage() {
   return (
     <div>
       <AboutHero />
       <AboutVision />
-<<<<<<< HEAD
       <AboutCreativeScroll />
       <Quotes />
       <AboutTeam />
       <AboutKnow />
-=======
       <Quotes />
       <AboutKnow />
       <AboutTeam />
->>>>>>> feature-25-contact-form
     </div>
   );
 }
