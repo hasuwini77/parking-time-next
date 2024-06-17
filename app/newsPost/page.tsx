23473788cc3ef615f87b39605de9f4b6c1624272
bedirgcmz <bdrgcmz@gmail.com>
2024-06-17 14:36:09 +0200

    Added to news page

--- app/newsPost/page.tsx
+++ app/newsPost/page.tsx
@@ -1,10 +1,12 @@
 import NewsHero from "@/components/NewsHero";
+import NewsSubscribe from "@/components/NewsSubscribe";
 import OurNews from "@/components/OurNews";
 
 const myPost = () => {
   return (
     <div>
       <NewsHero />
+      <NewsSubscribe />
       <OurNews />
     </div>
   );
