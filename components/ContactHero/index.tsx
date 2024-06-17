f51fd0f9143f8e23b459f9f0b82507b9cfefd45e
Noah Gordon <noah.gordon@edu.cmeducations.se>
2024-06-17 14:14:54 +0200

    added pl-24 on heading

--- components/ContactHero/index.tsx
+++ components/ContactHero/index.tsx
@@ -4,8 +4,8 @@ import { useLanguage } from "@/context/LanguageContext"
 const ContactHero: React.FC = () => {
     const { language } = useLanguage();
     return (
-        <div className="hidden w-full min-h-[60vh] relative bg-cover bg-[50%_50%] bg-[url('/images/contactHero.png')] xl:flex overflow-hidden">
-            <div className="w-full md:w-[44%] min-h-[60vh] lg:w-2/5 h-full bg-[rgba(0,0,0,0.2)] backdrop-blur-[2px] md:backdrop-blur-[10px] p-5 flex flex-col justify-center items-start text-white">
+        <section className="hidden w-full min-h-[60vh] relative bg-cover bg-[50%_50%] bg-[url('/images/contactHero.png')] xl:flex overflow-hidden">
+            <div className="w-full md:w-[44%] min-h-[60vh] lg:w-2/5 h-full bg-[rgba(0,0,0,0.2)] backdrop-blur-[2px] md:backdrop-blur-[10px] p-5 pl-24 flex flex-col justify-center items-start text-white">
                 <span className="list-disc text-xl list-inside pl-0 ml-0">
                     {language === "english" ? (
                         "Contact"
@@ -27,7 +27,7 @@ const ContactHero: React.FC = () => {
                     )} 
                 </span>
             </div>
-        </div>
+        </section>
     )
 }
 
