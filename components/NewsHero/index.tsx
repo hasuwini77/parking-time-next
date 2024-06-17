71ef79f1782d22edb281ae8a1432cab1fd3ba51e
Memai2023 <maria.hong26@gmail.com>
2024-06-17 17:55:36 +0200

    changed codes

--- components/NewsHero/index.tsx
+++ components/NewsHero/index.tsx
@@ -1,13 +1,22 @@
 'use client'
 import { useLanguage } from "@/context/LanguageContext"
+import Image from "next/image";
 
 const NewsHero: React.FC = () => {
     const { language } = useLanguage();
     return (
-        <div className="w-full short:min-h-[60vh] relative bg-cover bg-[50%_50%] bg-[url('/images/newsHero.png')] flex justify-start items-start overflow-hidden">
+        <div className="w-full short:min-h-[60vh] relative flex justify-start items-start overflow-hidden">
+            <Image
+                src="/images/newsHero.png"
+                alt="News Hero"
+                fill={true}
+                style={{objectFit: "cover"}}
+                quality={75}
+                priority
+                className="z-0"
+            />
             <div className="w-full short:min-h-[60vh] md:min-w-[350px] md:w-2/5 min-h-[50vh] bg-[rgba(0,0,0,0.2)] backdrop-blur-[2px] md:backdrop-blur-[10px] p-5 flex flex-col justify-center items-start text-white">
                 <span className="list-disc text-xl list-inside pl-0 ml-0">
-                    <span className="text-red text-[27px]">&bull;&nbsp;</span>
                     {language === "english" ? (
                         "News"
                     ) : (
