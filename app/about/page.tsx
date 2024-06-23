de51eb869bc4d8018a63c986087a97d2dba3463c
Edwin Andermyr <andermyredwin@gmail.com>
2024-06-23 16:26:23 +0200

    removed the creative scroll part

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,14 +3,12 @@ import AboutVision from "@/components/AboutVision";
 import Quotes from "@/components/AboutQuotes";
 import AboutKnow from "@/components/AboutKnow";
 import AboutTeam from "@/components/AboutTeam";
-import AboutCreativeScroll from "@/components/AboutCreativeScroll";
 
 export default function AboutPage() {
   return (
     <div>
       <AboutHero />
       <AboutVision />
-      <AboutCreativeScroll />
       <Quotes />
       <AboutTeam />
       <AboutKnow />
