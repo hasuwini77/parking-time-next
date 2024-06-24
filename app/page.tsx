226e81f2c4ea8626ab302c2745f472bf3518b966
Memai2023 <maria.hong26@gmail.com>
2024-06-24 23:26:59 +0200

    changed order in home

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,15 @@
 import HomeHero from "@/components/HomeHero";
-import HomeHowWorks from "@/components/HomeHowWorks";
-import HomeStatistics from "@/components/HomeStatistics";
-import TrustedLogo from "@/components/TrustedLogo";
-import HomeKnow from "@/components/HomeKnow";
-import Faq from "@/components/Faq";
 import SmartParking from "@/components/SmartParking";
+import HomeHowWorks from "@/components/HomeHowWorks";
 import HomeWhyParking from "@/components/HomeWhyParking";
-import HomeDownload from "@/components/Home-Download";
-import OurNews from "@/components/OurNews";
 import HomeCustomer from "@/components/HomeCustomer";
+import TrustedLogo from "@/components/TrustedLogo";
 import NewsSubscribe from "@/components/NewsSubscribe";
+import OurNews from "@/components/OurNews";
+import HomeDownload from "@/components/Home-Download";
+import HomeStatistics from "@/components/HomeStatistics";
+import Faq from "@/components/Faq";
+import HomeKnow from "@/components/HomeKnow";
 
 export default function Home() {
   return (
@@ -20,12 +20,12 @@ export default function Home() {
       <HomeWhyParking />      
       <HomeCustomer />
       <TrustedLogo />
-      <HomeStatistics />
+      <NewsSubscribe />
+      <OurNews />  
       <HomeDownload />
-      <OurNews />
-      <NewsSubscribe />      
-      <HomeKnow />
-      <Faq />      
+      <HomeStatistics />   
+      <Faq />
+      <HomeKnow />      
     </section>
   );
 }
