447f0b3d3fd4ead99aa8e2f32353bf519a83a9f5
Edwin Andermyr <andermyredwin@gmail.com>
2024-06-24 14:18:43 +0200

    adjusted the vercel issue

--- components/AboutValues/index.tsx
+++ components/AboutValues/index.tsx
@@ -1,76 +1,90 @@
-'use client';
-import React from 'react';
+"use client";
+import React from "react";
 import { useLanguage } from "@/context/LanguageContext";
-import Image from 'next/image';
-import Box from '@/public/images/aboutbox.png'
-import Check from '@/public/images/abouthuman.png'
-import Light from '@/public/images/aboutlight.png'
+import Image from "next/image";
+import Box from "@/public/images/aboutbox.png";
+import Check from "@/public/images/abouthuman.png";
+import Light from "@/public/images/aboutlight.png";
 
 const Values: React.FC = () => {
-    const { language } = useLanguage();
+  const { language } = useLanguage();
 
-    return (
-        <>
-        <section className="flex justify-center items-center w-full flex-col md:flex-col md:w-full md:items-center"> 
-                {language === "english" ? ( 
-                <span className="flex justify-center text-center flex-col md:mt-6">
-                    <h3 className="text-heading2 text-bold m-2.5">Our Values</h3>
-                    <p className="text-heading4 m-2.5">What we believe in.</p>
-                </span>
-                ) : (
-                <span> 
-                    <h3 className="text-heading2 text-bold m-2.5">Våra Värderinga</h3>
-                    <p className="text-heading4 m-2.5">De saker vi tror på</p>
-                </span>
-                )}    
-            <div className="flex w-2/4 justify-center items-center flex-col md:flex-row md:w-4/5 md:justify-center"> 
+  return (
+    <section className="flex flex-col items-center w-full pt-4 pb-4">
+      {language === "english" ? (
+        <div className="text-center flex flex-col mt-6">
+          <h3 className="text-heading2 font-bold m-2.5">Our Values</h3>
+          <p className="text-heading4 m-2.5">What we believe in.</p>
+        </div>
+      ) : (
+        <div className="text-center flex flex-col mt-6">
+          <h3 className="text-heading2 font-bold m-2.5">Våra Värderingar</h3>
+          <p className="text-heading4 m-2.5">De saker vi tror på</p>
+        </div>
+      )}
 
-                    <div className="justify-center items-center md:justify-content md:items-center md:flex md:flex-col md:m-2.5">
-                    <Image className="md:w-4/5" src={Box} alt="Box" rel="noreferrer"/> 
-                    {language === "english" ? (                    
-                    <span className="text-center mb-6 w-full justify-center items-center flex flex-col md:flex-col md:m-2.5 md:mb-10">                        
-                        <b className="text-xl">Simplicity</b>
-                        <p className="md:max-w-[200px]">We are striving to create simple and smooth experiences.</p>
-                    </span>
-                ) : (
-                    <span className="text-center mb-6 w-full justify-center items-center flex flex-col md:flex-col md:m-2.5 md:mb-10"> 
-                        <b className="text-xl">Enkelhet</b>
-                        <p className="md:max-w-[200px]">Vi strävar efter att skapa enkla och smidiga upplevelser.</p>
-                    </span>
-                )}                
-                    </div> 
-                    <div className="md:justify-content md:items-center md:flex md:flex-col md:m-2.5">
-                    <Image className="md:w-4/5" src={Light} alt="light" rel="noreferrer"/>
-                    {language === "english" ? (                    
-                    <span className="text-center mb-6 w-full justify-center items-center flex flex-col md:flex-col md:m-2.5 md:mb-10">                        
-                        <b className="text-xl">Innovation</b>
-                        <p className="md:max-w-[200px]">Always trying to improve and making our app a leader in its field.</p>
-                    </span>
-                ) : (
-                    <span className="text-center mb-6 w-full justify-center items-center flex flex-col md:flex-col md:m-2.5 md:mb-10"> 
-                        <b className="text-xl">Innovation</b>
-                        <p className="md:max-w-[200px]">Alltid sträva efter att förbättra och göra vår app ledande inom sitt område.</p>
-                    </span>
-                )}                    
-                    </div>
-                    <div className="md:justify-content md:items-center md:flex md:flex-col md:m-2.5">
-                    <Image className="md:w-4/5" src={Check} alt="check" rel="noreferrer"/>
-                    {language === "english" ? (                    
-                    <span className="text-center mb-6 w-full justify-center items-center flex flex-col md:flex-col md:m-2.5 md:mb-10">                        
-                        <b className="text-xl">User friendly</b>
-                        <p className="md:max-w-[200px]">It's crucial that our service meets users needs and enhances their parking experience.</p>
-                    </span>
-                ) : (
-                    <span className="text-center mb-6 w-full justify-center items-center flex flex-col md:flex-col md:mt-6 md:mb-10"> 
-                        <b className="text-xl">Användarvänlig</b>
-                        <p className="md:max-w-[200px]">Det är avgörande att vår tjänst möter användarnas behov och förbättrar deras parkeringsupplevelse.</p>
-                    </span>
-                )}
-                </div>
-                </div>
-        </section>
-        </>
-    )
-}
+      <div className="flex flex-col md:flex-row w-full md:w-4/5 justify-center items-stretch">
+        <div className="flex flex-col justify-center items-center m-2.5 w-full md:w-1/3">
+          <Image className="w-4/5 md:w-[80%]" src={Box} alt="Box" />
+          {language === "english" ? (
+            <div className="text-center flex flex-col justify-center items-center mt-2">
+              <p className="text-bold text-xl">Simplicity</p>
+              <p className="md:max-w-[200px] mt-2">
+                We are striving to create simple and smooth experiences.
+              </p>
+            </div>
+          ) : (
+            <div className="text-center flex flex-col justify-center items-center mt-2">
+              <p className="text-bold text-xl">Enkelhet</p>
+              <p className="md:max-w-[200px] mt-2">
+                Vi strävar efter att skapa enkla och smidiga upplevelser.
+              </p>
+            </div>
+          )}
+        </div>
+        <div className="flex flex-col justify-center items-center m-2.5 w-full md:w-1/3">
+          <Image className="w-4/5 md:w-[80%]" src={Light} alt="Light" />
+          {language === "english" ? (
+            <div className="text-center flex flex-col justify-center items-center mt-2">
+              <p className="text-bold text-xl">Innovation</p>
+              <p className="md:max-w-[200px] mt-2">
+                Always trying to improve and making our app a leader in its
+                field.
+              </p>
+            </div>
+          ) : (
+            <div className="text-center flex flex-col justify-center items-center mt-2">
+              <p className="text-bold text-xl">Innovation</p>
+              <p className="md:max-w-[200px] mt-2">
+                Alltid sträva efter att förbättra och göra vår app ledande inom
+                sitt område.
+              </p>
+            </div>
+          )}
+        </div>
+        <div className="flex flex-col justify-center items-center m-2.5 w-full md:w-1/3">
+          <Image className="w-4/5 md:w-[80%]" src={Check} alt="Check" />
+          {language === "english" ? (
+            <div className="text-center flex flex-col justify-center items-center mt-2">
+              <p className="text-bold text-xl">User friendly</p>
+              <p className="md:max-w-[200px] mt-2">
+                It&#39;s crucial that our service meets users&#39; needs and
+                enhances their parking experience.
+              </p>
+            </div>
+          ) : (
+            <div className="text-center flex flex-col justify-center items-center mt-2">
+              <p className="text-bold text-xl">Användarvänlig</p>
+              <p className="md:max-w-[200px] mt-2">
+                Det är avgörande att vår tjänst möter användarnas behov och
+                förbättrar deras parkeringsupplevelse.
+              </p>
+            </div>
+          )}
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Values
+export default Values;
